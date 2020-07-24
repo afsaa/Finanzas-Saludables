89cc3728c6d29c4be6f043ae6411bb919af8c369
Andrés Fernando Saa <andressaa.dev@gmail.com>
2020-07-24 15:44:16 -0500

    Agregando estructura, estilos y recursos del componente Carousel

--- client/src/Containers/App.tsx
+++ client/src/Containers/App.tsx
@@ -4,13 +4,52 @@ import { Header } from "../Components/Header";
 import { Footer } from "../Components/Footer";
 import { Login } from "../Components/Login";
 import { Register } from "../Components/Register";
+import { Carousel } from "../Components/Carousel";
 import "../assets/styles/App.scss";
+
+const homepagePosts: any = [
+  {
+    id: 1,
+    title: "Las deudas te ahogan?",
+    subtitle: `Los intereses que pagas en tus deudas cuentan y te pueden
+            perjudicar.`,
+    imgSrc:
+      "https://images.unsplash.com/photo-1500099817043-86d46000d58f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+    imgAlt: "Person Drowning",
+  },
+  {
+    id: 2,
+    title: "Aprende a crear tu presupuesto",
+    subtitle: `Determina en que estas gastando tu dinero y que cantidad.`,
+    imgSrc:
+      "https://images.unsplash.com/photo-1554224155-8d04cb21cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    imgAlt: "Budgeting",
+  },
+  {
+    id: 3,
+    title: "Desarrolla el habito de ahorrar",
+    subtitle: `Ya sea para obtener lo que quieres o para alcanzar tus metas.`,
+    imgSrc:
+      "https://images.unsplash.com/photo-1567427013953-88102117053a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+    imgAlt: "Saving",
+  },
+  {
+    id: 4,
+    title: "Invierte como debe ser",
+    subtitle: `La mayoría de personas deberían invertir siempre a largo plazo
+                    y de manera consistente.`,
+    imgSrc:
+      "https://images.unsplash.com/photo-1556155092-8707de31f9c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    imgAlt: "Investing",
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Register />
+      <Hero />
+      <Carousel posts={homepagePosts} />
       <Footer />
     </div>
   );
