98b0fce8be57170abb5d969fc3698ee93e98d7ec
Andrés Fernando Saa <andressaa.dev@gmail.com>
2020-08-04 13:05:52 -0500

    Creando cta en el componente Hero y modificando mixin de boton junto con sus llamados en otros estilos

--- client/src/Components/Hero.tsx
+++ client/src/Components/Hero.tsx
@@ -6,12 +6,16 @@ export const Hero = () => {
     "45% de los empleados gastan entre 2 y 3 horas preocupándose por su situación financiera";
   const hero_description_text: String =
     "Ahorra esas horas para usarlas en lo que más te gusta.";
+  const hero_cta_text: String = "Aprende ahora como lograrlo!";
 
   return (
     <div className="hero">
       <div className="hero__container">
         <h2 className="hero__title">{hero_title_text}</h2>
         <p className="hero__description">{hero_description_text}</p>
+        <button className="hero__cta" type="button">
+          {hero_cta_text.toUpperCase()}
+        </button>
       </div>
     </div>
   );
